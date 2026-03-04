2185ccd1a7284448830a3d4a93a2f78fff150be0
Chaineze Said <chachanam24@gmail>
2026-03-05 00:36:53 +0100

    Premier Besoin

---
+++ src/main.ts
@@ -0,0 +1,40 @@
+interface Commande {
+    id: number;
+    nom: string;
+    description: string;
+    prix: number;
+
+}
+
+const maCommande: Commande[] = [
+    {
+        id: 1,
+        nom: "Anchois 23cm",
+        description: "sauce tomate premium, origan, huile d'olive extra vierge, anchois, olive",
+        prix: 7.9
+
+    },
+
+    {
+        id: 2,
+        nom: "Emmental 23cm",
+        description: "sauce tomate premium, origan, huile d'olive extra vierge, emmental, basilic, olive",
+        prix: 7.9
+    },
+
+    {
+        id: 3,
+        nom: "Margherita 23cm",
+        description: "sauce tomate premium, origan, huile d'olive extra vierge, mozzarella",
+        prix: 7.9
+    }
+]
+    
+
+
+
+console.log(maCommande); // pour afficher en console les données. 
+
+
+
+
