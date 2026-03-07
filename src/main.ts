219383ea0677e65c1b2d4c47bba261d32f3346d9
Chaineze Said <chachanam24@gmail.com>
2026-03-07 15:39:10 +0100

    Deuxième Besoin

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ interface Commande {
 
 }
 
-const maCommande: Commande[] = [
+const commandes: Commande[] = [
     {
         id: 1,
         nom: "Anchois 23cm",
@@ -34,10 +34,34 @@ const maCommande: Commande[] = [
     }
 ]
 
+// ``
 
-console.log(maCommande); // pour afficher en console les données. 
+console.log(commandes); // pour afficher en console les données. 
 
 
+// On précise <HTMLDivElement> pour que TS connaisse les propriétés de la DIV
 
+const appDiv = document.querySelector<HTMLDivElement>('#app')
 
+// "if" est une sécutité indispensable ici
+if (appDiv) {
+    appDiv.innerHTML= `
+    
+    ${commandes.map(c => {
+
+        return `
+        <div class="card">
+            <h3>${c.nom}</h2>
+            <p>${c.description}</p>
+            <p><strong> Prix : ${c.prix} €</strong></p>
+            
+        </div>`
+
+        // Utilisation du join pour "coller" tous les éléments du tableau sans aucun séparateur 
+    }).join('')
+    } 
+
+       
+    `;
+}
 
