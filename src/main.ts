a675f031f288d44da144891a645f0fadb137852a
Chaineze Said <chachanam24@gmail.com>
2026-03-13 18:36:06 +0100

    Besoin no 2

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+// Afin de pouvoir avoir acces a mon api j'ai mis une en-tete sur le fichier index.php du dosier eatsmart-chaineze
+
 import './style.css'
 
 
@@ -10,22 +12,52 @@ interface Article {
 
 }
 
+// Fonction qui récupère les articles depuis l'api ou y'a la BDD
 
 async function fetchArticle(): Promise<Article[]> {
-
+    
      const res = await fetch('http://localhost/eatsmart-chaineze/articles');
      return await res.json();
 }
 
+ 
+// Fonction principale avec le code qui sera afficher 
 async function init() {
     console.log("Chargement des données...");
 
     const articles = await fetchArticle();
 
+    // Affichage dans la console 
     console.log(articles);
 
+
+    // On précise <HTMLDivElement> pour que TS connaisse les propriétés de la DIV
+    const appDiv = document.querySelector<HTMLDivElement>('#app')
+
+    // "if" est une sécutité indispensable ici
+    if (appDiv) {
+        appDiv.innerHTML= `
+    
+        ${articles.map(c => {
+
+            // Pour chaque article on crée une carte
+            return `
+            <div class="card">
+                <h3>${c.nom}</h2>
+                <p>${c.description}</p>
+                <p><strong> Prix : ${c.prix} €</strong></p>
+                
+            </div>`
+
+            // Utilisation du join pour "coller" tous les éléments du tableau sans aucun séparateur 
+        }).join('')
+    } 
+
+    `;
 }
 
+}
+// Affichage 
 init();
 
 
