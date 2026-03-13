b72dd63d7107e3dfd0d075c1955a5907aff24aff
Chaineze Said <chachanam24@gmail.com>
2026-03-13 18:06:08 +0100

    Besoin no 1

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './style.css'
 
 
 
-interface Commande {
+interface Article {
     id: number;
     nom: string;
     description: string;
@@ -11,59 +11,30 @@ interface Commande {
 }
 
 
-const commandes: Commande[] = [
-    {
-        id: 1,
-        nom: "Anchois 23cm",
-        description: "sauce tomate premium, origan, huile d'olive extra vierge, anchois, olive",
-        prix: 7.9
+async function fetchArticle(): Promise<Article[]> {
 
-    },
-
-    {
-        id: 2,
-        nom: "Emmental 23cm",
-        description: "sauce tomate premium, origan, huile d'olive extra vierge, emmental, basilic, olive",
-        prix: 7.9
-    },
-
-    {
-        id: 3,
-        nom: "Margherita 23cm",
-        description: "sauce tomate premium, origan, huile d'olive extra vierge, mozzarella",
-        prix: 7.9
-    }
-]
-
-
-// ``
-
-console.log(commandes); // pour afficher en console les données. 
-
-
-// On précise <HTMLDivElement> pour que TS connaisse les propriétés de la DIV
-
-const appDiv = document.querySelector<HTMLDivElement>('#app')
-
-// "if" est une sécutité indispensable ici
-if (appDiv) {
-    appDiv.innerHTML= `
-    
-    ${commandes.map(c => {
-
-        return `
-        <div class="card">
-            <h3>${c.nom}</h2>
-            <p>${c.description}</p>
-            <p><strong> Prix : ${c.prix} €</strong></p>
-            
-        </div>`
-
-        // Utilisation du join pour "coller" tous les éléments du tableau sans aucun séparateur 
-    }).join('')
-    } 
-
-       
-    `;
+     const res = await fetch('http://localhost/eatsmart-chaineze/articles');
+     return await res.json();
 }
+
+async function init() {
+    console.log("Chargement des données...");
+
+    const articles = await fetchArticle();
+
+    console.log(articles);
+
+}
+
+init();
+
+
+
+
+
+
+
+
+
+
 
